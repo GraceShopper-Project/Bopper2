74e41c0d2fe692c09765b6621c91a694b4fc2464
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-16 18:03:23 -0400

    product routes and gateKeepingMiddleware file added

---
+++ server/api/products.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const {
+  models: { Product },
+} = require("../db");
+
+module.exports = router;
+
+//GET /api/products
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll({});
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET /api/products/:prodId
+router.get("/:prodId", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.prodId);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//POST /api/products/
+router.post("/", async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      return res.status(403).send("You Shall not pass!");
+    }
+    res.status(201).send(await Product.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+//PUT /api/products/:prodId
+router.put("/:prodId", async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      return res.status(403).send("You Shall not pass!");
+    }
+    const product = await Product.findbyPk(req.params.prodId);
+    res.status(202).send(await Product.update(product));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/products/:prodId
+router.delete("/:prodId", async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      return res.status(403).send("You Shall not pass!");
+    }
+    const product = await Product.findbyPk(req.params.prodId);
+    await product.destroy();
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
