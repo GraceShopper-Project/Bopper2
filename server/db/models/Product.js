66e99667654ea1544b346cc6c86030933faf9f2e
Clare Sweeny <clareesweeny@gmail.com>
2021-06-16 16:13:01 -0400

    working sequelize models

---
+++ server/db/models/Product.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+    name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      }
+})
+
+module.exports = Product
