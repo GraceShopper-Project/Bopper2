c2bee60ea107d2322cc49e259b0e2930e52b0468
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-17 18:33:24 -0400

    Merge branch 'main' of https://github.com/GraceShopper-Project/Bopper2 into #20-product-component

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,11 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 import productsReducer from './products'
+import allUsers from './allUsers'
 
-const reducer = combineReducers({ auth, productsReducer })
+const reducer = combineReducers({ auth, productsReducer, allUsers })
+
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
