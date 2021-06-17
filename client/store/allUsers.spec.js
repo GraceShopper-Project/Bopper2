dfcaceb2e7137553514fdd81b04eaf194c37aef1
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-17 19:02:40 -0400

    removed .only from spec

--- client/store/allUsers.spec.js
+++ client/store/allUsers.spec.js
@@ -7,7 +7,7 @@ import { expect } from 'chai'
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
-describe.only('AllUsers', () => {
+describe('AllUsers', () => {
     describe('fetchUsers thunk', () => {
         afterEach(() => {
             fetchMock.restore()
