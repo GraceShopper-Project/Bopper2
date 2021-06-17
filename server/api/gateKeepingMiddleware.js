7c2fcfd38ec19b028b1f13e4346a9ead3e51385b
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-17 10:30:02 -0400

    added next() to keep middleware flowing

---
+++ server/api/gateKeepingMiddleware.js
@@ -0,0 +1,27 @@
+const {
+  models: { User },
+} = require("../db");
+
+//here we make a function so we can  make sure a user is logged in before performing functions.
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return res.status(403).send("You Shall not pass!");
+  }
+  //would also export this and require and include it on user route
+};
+
+module.exports = {
+  requireToken,
+  isAdmin,
+};
