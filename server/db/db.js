a689e311ed1b512808c5af6df355a3348121823b
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-06-18 05:54:42 -0400

    (optional) debug logging

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
+const debug = require('debug')('app:db:init')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
@@ -23,6 +24,12 @@ if(process.env.DATABASE_URL){
 const USER = process.env.DB_USER || process.env.USER
 const PASS = process.env.DB_PASS || ''
 
+debug('connecting to db')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://${USER}:${PASS}@localhost:5432/${databaseName}`, config)
 module.exports = db
+
+process.on('exit', () => {
+  debug('closing db')
+  db.close()
+})
