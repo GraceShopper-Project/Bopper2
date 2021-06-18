41e87698a4334ef4965200b64c0962c2f6ebc291
Clare Sweeny <clareesweeny@gmail.com>
2021-06-18 13:46:09 -0400

    changed users route to return email

--- server/api/users.js
+++ server/api/users.js
@@ -13,7 +13,7 @@ router.get("/", isAdmin, async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ["id", "name"],
+      attributes: ["id", "name", "email"],
     });
     debug(`returning ${users.length} users`)
     res.json(users);
