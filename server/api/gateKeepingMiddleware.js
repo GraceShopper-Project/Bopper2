94a4c45f7da6fe5f5fcaf10c313109a86d9c93af
Clare Sweeny <clareesweeny@gmail.com>
2021-06-18 13:34:54 -0400

    Reformatted gkmiddleware and user model

--- server/api/gateKeepingMiddleware.js
+++ server/api/gateKeepingMiddleware.js
@@ -4,13 +4,15 @@ const {
 
 //here we make a function so we can  make sure a user is logged in before performing functions.
 const requireToken = async (req, res, next) => {
+
   try {
     const token = req.headers.authorization;
     const user = await User.findByToken(token);
-    req.user = user;
+    if(!user) throw new Error('No User Found')
+    req.user = user.dataValues;
     next();
   } catch (error) {
-    next(error);
+    throw error
   }
 };
 
@@ -19,7 +21,6 @@ const isAdmin = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return res.status(403).send("You Shall not pass!");
   }
-  //would also export this and require and include it on user route
   next()
 };
 
