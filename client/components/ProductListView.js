0118cd5dd1a5e5620693fb4fb15d507737d745c1
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-19 11:04:17 -0400

    product display component split from AllProducts

---
+++ client/components/ProductListView.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export const ProductListView = (props) =>{
+   return(
+   <div className="products">
+      {props.products.map((product) => {
+         return (
+            <div key={product.id}>
+               <h1>
+               <Link to={`/products/${product.id}`}>
+                  {product.name}
+               </Link>
+               </h1>
+               <i>
+                  <h4>
+                     <p>{product.description}</p>
+                     <p>${(product.price / 100).toFixed(2)}</p>
+                     <img src={product.imageUrl} />
+                  </h4>
+               </i>
+            </div>
+         );
+      })}
+   </div>
+   )
+}
