ede70fc527a81d413b556ad5e164c653dfe22446
Fiora Wise <fiorathewise@gmail.com>
2021-06-19 18:10:47 -0400

    Adds add to cart to store and reducer

--- client/store/singleUser.js
+++ client/store/singleUser.js
@@ -3,15 +3,24 @@
  */
 export const actionTypes = {
   SET_USER: "SU_SETUSER",
+  ADD_TO_CART: "SU_ADD_TO_CART",
 };
 
 /**
  * Action Creators
  */
-const setUser = (user) => {
+export const setUser = (user) => {
   return {
     type: actionTypes.SET_USER,
     user,
+  };
+};
+
+const _addToCart = (productId, quantity) => {
+  return {
+    type: actionTypes.SU_ADD_TO_CART,
+    productId,
+    quantity,
   };
 };
 
@@ -33,13 +42,43 @@ export const fetchUser = (userId) => async (dispatch) => {
   }
 };
 
+export const addToCart =
+  (productId, quantity) => async (dispatch, getState) => {
+    try {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        fetch(`/api/users/${userId}/cart`, {
+          method: "PUT",
+          headers: {
+            authorization: token,
+          },
+          body: getState().user.cart,
+        });
+      }
+      dispatch(_addToCart(productId, quantity));
+    } catch (err) {
+      throw err;
+    }
+  };
+
 /**
  * REDUCER
  */
-export default function (state = [], action) {
+export default function (state = {}, action) {
   switch (action.type) {
     case actionTypes.SET_USER:
       return action.user;
+    case actionTypes.ADD_TO_CART:
+      return {
+        ...state,
+        cart: [
+          ...state.cart,
+          {
+            productId: action.productId,
+            quantity: action.quantity,
+          },
+        ],
+      };
     default:
       return state;
   }
