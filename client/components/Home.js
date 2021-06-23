23ce4823a2427bcfda47e3a4f2c4798243f64768
Clare Sweeny <clareesweeny@gmail.com>
2021-06-23 19:06:41 -0400

    Working carousel in Home.js

--- client/components/Home.js
+++ client/components/Home.js
@@ -16,6 +16,45 @@ export const Home = props => {
       }   
       <h3>Your one stop bop shop where all the beats drop</h3>
       <p>We sell a wide variety of speakers and headphones</p>
+
+      <div id="carouselImages" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="./img/StudioMonitors.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/Subwoofer.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/BluetoothBeachSpeaker.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/BluetoothSpeakerForKids.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/CenterChannelSpeaker.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/DolbyAtomsCeilingSpeakers.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/FancyHeadphones.jpg" className="d-block w-100" />
+          </div>
+          <div className="carousel-item">
+            <img src="./img/SurroundSpeakers.jpg" className="d-block w-100" />
+          </div>
+
+            {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button> */}
+        </div>
+      </div>
+
     </div>
   )
 }
