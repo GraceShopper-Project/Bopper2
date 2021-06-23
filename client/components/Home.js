8a12ed8d46a57d37117228625876bb42b2c4fd6d
Fiora Wise <fiorathewise@gmail.com>
2021-06-23 17:25:34 -0400

    Fixes home tests

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,14 +5,13 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const Home = props => {
-  console.log(props)
   const {name} = props
 
   return (
     <div className="home-body">
       {
         name ? 
-        <h3>Welcome, {name} to Grace Bopper!</h3> :
+        <h3 id="name">Welcome, {name} to Grace Bopper!</h3> :
         <h3>Welcome to Grace Bopper!</h3>
       }   
       <h3>Your one stop bop shop where all the beats drop</h3>
