9adbc4d28bcbec959cf46240c2bb1143f48bf7d3
Fiora Wise <fiorathewise@gmail.com>
2021-06-23 17:03:36 -0400

    Styles single product view card

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,35 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getSingleProduct } from "../store/singleProduct";
-import AddToCartButton from './AddToCartButton'
 import {addToCart} from '../store/singleUser'
+import ProductCard from "./ProductCard"
 
 class SingleProduct extends React.Component {
-   constructor(props){
-      super(props)
-   }
 
    componentDidMount() {
       const singleProduct = this.props.match.params.prodId
       this.props.setProduct(singleProduct);
    }
 
-   render() {
-      const product = this.props.product || {};
-      return (
-         <div className="product">
-            <div key={product.id}>
-               <h1>{product.name}</h1>
-                  <h4>
-                     <p>{product.description}</p>
-                     <p>${(product.price / 100).toFixed(2)}</p>
-                     <img src={product.imageUrl} />
-                  </h4>
-            </div>
-            <AddToCartButton onClick={() => this.props.addToCart(product)}/>
-         </div>
-      );
-   }
+  render() {
+    const product = this.props.product || [];
+    return (
+      <div id="single-product">
+        <ProductCard 
+          {...this.props}
+          key={product.id}
+          product={product} 
+          addToCart={this.props.addToCart}
+        />
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => {
